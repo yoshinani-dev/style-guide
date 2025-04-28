43549f6eb49363c4dbeb2e2549a3ef741872ea11
FukeKazki <kazkichi0906@gmail.com>
2025-04-28 18:57:17 +0900

    feat: react系のプラグインを追加

--- eslint/react-internal.mjs
+++ eslint/react-internal.mjs
@@ -2,16 +2,24 @@
 
 import base from "./base.mjs";
 import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
 const eslintConfig = [
   ...base,
   {
     languageOptions: {
       globals: {
+        ...globals.serviceworker,
         ...globals.browser,
       },
     },
     ignores: ["node_modules/", "dist/", "*.js"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
+    ...react.configs.flat.recommended,
+    ...reactHooks.configs["recommended-latest"],
   },
 ];
 
