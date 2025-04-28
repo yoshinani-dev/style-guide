6cad8cbe71bf3870714bcf4e5fd3120f7dea4b26
FukeKazki <kazkichi0906@gmail.com>
2025-04-28 20:38:49 +0900

    feat: prettierの設定をガイドラインに追加

---
+++ prettier/prettier.config.cjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  endOfLine: "lf",
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  singleQuote: false,
+  semi: false,
+  trailingComma: "all",
+};
+
+module.exports = config;
