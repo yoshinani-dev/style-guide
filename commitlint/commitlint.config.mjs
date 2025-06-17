f2682c6d4911850eb392eb6353647ada6af71d8b
FukeKazki <kazkichi0906@gmail.com>
2025-06-17 19:28:00 +0900

    feat: Apple

--- commitlint/commitlint.config.mjs
+++ commitlint/commitlint.config.mjs
@@ -5,28 +5,27 @@ export default {
       2,
       "always",
       [
-        // ビルドシステムや依存関係のみの変更
-        "build",
-        // CIワークフローの変更
-        "ci",
-        // ドキュメントのみの変更
-        "docs",
-        // 新機能
-        "feat",
-        // バグ修正
-        "fix",
-        // パフォーマンス向上のためのコード変更
-        "perf",
-        // バグ修正や新機能追加以外のコード変更
-        "refactor",
-        // 以前のコミットの取り消し
-        "revert",
-        // コードの意味に影響しない変更
-        "style",
-        // テストの追加や既存テストの修正
-        "test",
-        // 自動リリース専用
-        "release",
+        "build", // ビルドシステムや依存関係のみの変更
+        "docs", // ドキュメントのみの変更
+        "feat", // 新機能
+        "fix", // バグ修正
+        "refactor", // バグ修正や新機能追加以外のコード変更
+        "test", // テストの追加や既存テストの修正
+        "release", // 自動リリース専用
+      ],
+    ],
+    "subject-case": [
+      0,
+      "never",
+      [
+        "lower-case", // default
+        "upper-case", // UPPERCASE
+        "camel-case", // camelCase
+        "kebab-case", // kebab-case
+        "pascal-case", // PascalCase
+        "sentence-case", // Sentence case
+        "snake-case", // snake_case
+        "start-case", // Start Case
       ],
     ],
   },
